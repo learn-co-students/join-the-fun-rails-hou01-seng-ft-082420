360cfa9520a34309a23985d7fded37efd590c75e
breewarren <ambreea.warren@gmail.com>
2020-09-22 13:24:19 -0500

    Done.

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -1,6 +1,9 @@
 class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
+      t.string :ride_name
+      t.integer :taxi_id
+      t.integer :passenger_id
       t.timestamps null: false
     end
   end
